d2cb5c2094afe093f0dc288c67da6dd2bb4f32af
nasrin haghighi <ns.haghighi@gmail.com>
2023-02-10 15:05:18 +0000

    controller func

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,25 +1,64 @@
 
 const Task = require('../models/Tasks')
 
-const getAllTasks=(req, res)=>{
-    res.send('get all tasks')
+const getAllTasks= async(req, res)=>{
+    try{
+        const task =await Task.find(req.body)
+        res.status(201).json({task})
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
 }
 
 
 const createTask= async  (req, res)=>{
-    const task =await Task.create(req.body)
-    res.status(201).json({task})
- 
+
+    try{
+        const task =await Task.create(req.body)
+        res.status(201).json({task})
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
+  }
+const getTask= async (req, res)=>{
+    try{
+        const {id:taskID} =req.params
+        const task =await Task.findOne({_id : taskID})
+        if(!task){
+            return res.status(404).json({mes:`No task with: ${taskID}`})
+        }
+        res.status(201).json({task})
+    }catch(error){
+        res.status(500).json({msg:error})
+    }
+  }
+
+const deletTask= async(req, res)=>{
+    try{
+        const {id:taskID}=req.params
+        const task= await Task.findByIdAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json({mes:`No task with: ${taskID}`})
+        }
+        res.status(200).json({task})
+       }catch(error){
+        res.status(500).json({msg:error})
+       }
 }
 
-const getTask=(req, res)=>{
-    res.json({id:req.params.id})
-}
-const updateTask=(req, res)=>{
-    res.send('update task')
-}
-const deletTask=(req, res)=>{
-    res.send('delete task')
+
+
+const updateTask= async(req, res)=>{
+ try{
+    const {id:taskID}=req.params
+    const task= await Task.findOneAndUpdate({_id :taskID}, req.body , {new:true, runValidators:true})
+    if(!task){
+        return res.status(404).json({mes:`No task with: ${taskID}`})
+    }
+    res.status(200).json({task})
+ }catch(error){
+
+ }
 }
 
 module.exports ={
--- models/Tasks.js
+++ models/Tasks.js
@@ -1,5 +1,17 @@
 const mongoose=require('mongoose')
 
-const taskSchema = new mongoose.Schema({ name: 'string', completed: 'boolean' });
+const taskSchema = new mongoose.Schema({ name:{
+    type:String,
+    required:[true, 'must provid a name'],
+    maxlength:[20, 'name can not be more than 20 ca'],
+    trim:true
+},
+
+    
+    
+   completed:{
+    type:Boolean,
+    default:false
+   } });
 
 module.exports =mongoose.model('Task', taskSchema)
